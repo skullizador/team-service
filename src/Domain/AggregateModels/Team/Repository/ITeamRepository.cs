40a878133ec3dd813b3737fa2af390bac966071e
Carlos Nunes <carlosnunes__9@outlook.pt>
2023-03-09 14:15:06 +0000

    HA-122: Add "CreateTeamAsync" feature to TeamController;

--- src/Domain/AggregateModels/Team/Repository/ITeamRepository.cs
+++ src/Domain/AggregateModels/Team/Repository/ITeamRepository.cs
@@ -9,6 +9,8 @@
 
 namespace TeamService.Domain.AggregateModels.Team.Repository
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using TeamService.Domain.SeedWork;
 
     /// <summary>
@@ -17,5 +19,12 @@ namespace TeamService.Domain.AggregateModels.Team.Repository
     /// <seealso cref="IRepository{Team}"/>
     public interface ITeamRepository : IRepository<Team>
     {
+        /// <summary>
+        /// Gets the by name asynchronous.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        Task<Team> GetByNameAsync(string name, CancellationToken cancellationToken);
     }
 }
---
+++ src/Presentation.WebAPI/Controllers/TeamController.cs
@@ -0,0 +1,73 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TeamController.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// TeamController
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TeamService.Presentation.WebAPI.Controllers
+{
+    using System.Net;
+    using AutoMapper;
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+    using TeamService.Domain.AggregateModels.Team;
+    using TeamService.Presentation.WebAPI.Commands.CreateTeamCommand;
+    using TeamService.Presentation.WebAPI.Dto.Input;
+    using TeamService.Presentation.WebAPI.Dto.Output;
+    using TeamService.Presentation.WebAPI.Utils;
+
+    /// <summary>
+    /// <see cref="TeamController"/>
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller"/>
+    [ApiController]
+    [Route("api/v1/Team")]
+    public class TeamController : Controller
+    {
+        /// <summary>
+        /// The mapper
+        /// </summary>
+        private readonly IMapper mapper;
+
+        /// <summary>
+        /// The mediator
+        /// </summary>
+        private readonly IMediator mediator;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TeamController"/> class.
+        /// </summary>
+        /// <param name="mapper">The mapper.</param>
+        /// <param name="mediator">The mediator.</param>
+        public TeamController(
+            IMapper mapper,
+            IMediator mediator)
+        {
+            this.mapper = mapper;
+            this.mediator = mediator;
+        }
+
+        /// <summary>
+        /// Creates the team asynchronous.
+        /// </summary>
+        /// <param name="createTeamDto">The create team dto.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(TeamDetailsDto), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> CreateTeamAsync([FromBody] CreateTeamDto createTeamDto, CancellationToken cancellationToken)
+        {
+            Team team = await this.mediator.Send(new CreateTeamCommand
+            {
+                Name = createTeamDto.Name,
+                ShortName = createTeamDto.ShortName,
+            }, cancellationToken);
+
+            return this.Ok(this.mapper.Map<TeamDetailsDto>(team));
+        }
+    }
+}
